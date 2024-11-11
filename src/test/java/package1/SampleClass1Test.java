20ab130620265450cefe00a9e728c3bef963bc4e
rajani315 <rajani.srinivas.a12@gmail.com>
2024-11-11 13:22:55 +0530

    First Commit

---
+++ src/test/java/package1/SampleClass1Test.java
@@ -0,0 +1,12 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class SampleClass1Test {
+	
+	@Test
+	public void method1Test() {
+		System.out.println("method1");
+	}
+
+}
---
+++ src/test/java/package2/SampleClass2Test.java
@@ -0,0 +1,12 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class SampleClass2Test {
+	
+	@Test
+	public void method2Test() {
+		System.out.println("method1");
+	}
+
+}
