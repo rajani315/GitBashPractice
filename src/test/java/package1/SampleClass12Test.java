0125ae075354ce58f0d309c6ab8bdf32c8a79235
rajani315 <rajani.srinivas.a12@gmail.com>
2024-11-12 13:26:41 +0530

    Changes in SampleClass12Test

--- src/test/java/package1/SampleClass12Test.java
+++ src/test/java/package1/SampleClass12Test.java
@@ -6,7 +6,17 @@ public class SampleClass12Test {
 
 	@Test
 	public void method3() {
-		System.out.println("heyyyyy");
+		
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+
 	}
 	
 }
